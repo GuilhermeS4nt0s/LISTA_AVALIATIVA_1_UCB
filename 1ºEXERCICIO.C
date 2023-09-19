6482a6c5d5d2fae8182678d54f68c791b2c24d39
Guilherme Santos <113079822+GuilhermeS4nt0s@users.noreply.github.com>
2023-09-19 17:54:58 -0300

    Começando a Lista Avaliativa

---
+++ 1ºEXERCICIO.C
@@ -0,0 +1,39 @@
+//Questao 1
+
+Faca um programa que verifique se em determinado ano ocorreram Jogos Olimpicos de Verao (da Era
+Moderna) e/ou Copa do Mundo de Futebol.
+
+Os anos de realizacao dos dois eventos podem ser encontrados nos seguintes links:
+Jogos Olimpicos de Verao
+
+Copa do Mundo de Futebol
+
+Entrada
+
+A entrada e um valor inteiro positivo X (1800 ≥ X ≥ 2022) representando um ano.
+
+Saida
+
+A saida do programa deve mostrar se ocorreu algum dos eventos no ano inserido X, da seguinte forma:
+
+- Caso nao tenha ocorrido nenhum dos eventos, deve ser mostrada a seguinte mensagem: ''Nao houve
+Jogos Olimpicos de Verao ou Copa do Mundo no ano de X.'';
+
+- Caso tenha ocorrido Olimpiadas no ano inserido, deve ser mostrada uma mensagem no seguinte
+formato: 'Os Jogos Olimpicos de Verao ocorreram no ano de X.';
+
+- Caso tenha ocorrido Copa do Mundo no ano inserido, deve ser mostrada uma mensagem no seguinte
+formato: 'A Copa do Mundo de Futebol ocorreu no ano de X.';
+
+Nao se esqueca de imprimir as mensagens com uma quebra de linha ao final.
+
+Exemplo de entrada Saida
+
+1800 Nao houve Jogos Olimpicos de Verao ou Copa do
+Mundo no ano de 1800.
+1934 A Copa do Mundo de Futebol ocorreu no ano de
+
+2016 Os Jogos Olimpicos de Verao ocorreram no ano de
+
+#include <stdio.h>
+
