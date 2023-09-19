bda2fb1812b858955327a35729354af1da4dd3d8
Guilherme Santos <113079822+GuilhermeS4nt0s@users.noreply.github.com>
2023-09-19 18:40:19 -0300

    iniciando exercicio 1

--- 1ºEXERCICIO.C
+++ 1ºEXERCICIO.C
@@ -1,39 +1,59 @@
 //Questao 1
 
-Faca um programa que verifique se em determinado ano ocorreram Jogos Olimpicos de Verao (da Era
-Moderna) e/ou Copa do Mundo de Futebol.
+//Faca um programa que verifique se em determinado ano ocorreram Jogos Olimpicos de Verao (da Era
+//Moderna) e/ou Copa do Mundo de Futebol.//
 
-Os anos de realizacao dos dois eventos podem ser encontrados nos seguintes links:
-Jogos Olimpicos de Verao
+//Os anos de realizacao dos dois eventos podem ser encontrados nos seguintes links:
+//Jogos Olimpicos de Verao
 
-Copa do Mundo de Futebol
+//Entrada
 
-Entrada
+//A entrada e um valor inteiro positivo X (1800 > X > 2022) representando um ano.//
 
-A entrada e um valor inteiro positivo X (1800 ≥ X ≥ 2022) representando um ano.
+//Saida
 
-Saida
+//A saida do programa deve mostrar se ocorreu algum dos eventos no ano inserido X, da seguinte forma:
 
-A saida do programa deve mostrar se ocorreu algum dos eventos no ano inserido X, da seguinte forma:
+//- Caso nao tenha ocorrido nenhum dos eventos, deve ser mostrada a seguinte mensagem: ''Nao houve
+//Jogos Olimpicos de Verao ou Copa do Mundo no ano de X.'';//
 
-- Caso nao tenha ocorrido nenhum dos eventos, deve ser mostrada a seguinte mensagem: ''Nao houve
-Jogos Olimpicos de Verao ou Copa do Mundo no ano de X.'';
+//- Caso tenha ocorrido Olimpiadas no ano inserido, deve ser mostrada uma mensagem no seguinte
+//formato: 'Os Jogos Olimpicos de Verao ocorreram no ano de X.';//
 
-- Caso tenha ocorrido Olimpiadas no ano inserido, deve ser mostrada uma mensagem no seguinte
-formato: 'Os Jogos Olimpicos de Verao ocorreram no ano de X.';
+//- Caso tenha ocorrido Copa do Mundo no ano inserido, deve ser mostrada uma mensagem no seguinte
+//formato: 'A Copa do Mundo de Futebol ocorreu no ano de X.';
 
-- Caso tenha ocorrido Copa do Mundo no ano inserido, deve ser mostrada uma mensagem no seguinte
-formato: 'A Copa do Mundo de Futebol ocorreu no ano de X.';
+//Nao se esqueca de imprimir as mensagens com uma quebra de linha ao final.
 
-Nao se esqueca de imprimir as mensagens com uma quebra de linha ao final.
+//Exemplo de entrada Saida//
 
-Exemplo de entrada Saida
+//1800 | Nao houve Jogos Olimpicos de Verao ou Copa do
+//Mundo no ano de 1800.//
+//1934 | A Copa do Mundo de Futebol ocorreu no ano de//
 
-1800 Nao houve Jogos Olimpicos de Verao ou Copa do
-Mundo no ano de 1800.
-1934 A Copa do Mundo de Futebol ocorreu no ano de
+//2016 | Os Jogos Olimpicos de Verao ocorreram no ano de//
 
-2016 Os Jogos Olimpicos de Verao ocorreram no ano de
+// ---------------------------//
+
+//COPA DO MUNDO OCORREU MENOS NOS ANOS 1942 e 1946//
+
+//COPA DO MUNDO OCORREU EM 1930//
+
+//Os Jogos Olimpicos de Verao  ocorreram em 1896//
+
+// if(nmrdacopa >= 1800)
+//Nao houve Jogos Olimpicos de Verao ou Copa do
+//Mundo no ano de 1800.
+
 
 #include <stdio.h>
 
+int main(){
+
+    int numerodequandoocorreuacopa = 1930 >= 2022;
+    int numerodequandoocorreuosjogosolimpicos =  1896 >= 2021;
+    int numerodequandonaoocorreuacopa = 1800 >= 1929;
+    int numerodequandonaoocorreuosjogosolimpicos = 1 >= 1895; 
+
+        printf("insira uma operacao:\n");
+}
