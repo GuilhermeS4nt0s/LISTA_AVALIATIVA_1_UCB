b6a49ca8bd1927af07957ddc3f844c0d9f969ae3
GuilhermeS4nt0s <guiguisantosaraujo@gmail.com>
2023-09-22 20:33:42 -0300

    começando ex 2

---
+++ 2ºEXERCICIO.C
@@ -0,0 +1,17 @@
+int main (){
+
+    int numero;
+
+    printf("Digite o ano que deseja verificar: ");
+    scanf("%d", &numero);
+    
+    if (numero %2 == 0){
+        
+         printf("O numero %d eh par", numero)
+    }
+    
+
+
+
+
+}
