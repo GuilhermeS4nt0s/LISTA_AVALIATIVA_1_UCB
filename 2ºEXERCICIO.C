b17220edbc0a3f8135938d7c7510f924d0eaebc3
Guilherme Santos <113079822+GuilhermeS4nt0s@users.noreply.github.com>
2023-09-26 16:37:43 -0300

    Update 2ºEXERCICIO.C

--- 2ºEXERCICIO.C
+++ 2ºEXERCICIO.C
@@ -1,17 +1,25 @@
-int main (){
+#include <stdio.h>
+#include <stdlib.h>
 
-    int numero;
+int main() {
+  int numero, numeroCopia;
+  int somadigitos = 0;
 
-    printf("Digite o ano que deseja verificar: ");
-    scanf("%d", &numero);
-    
-    if (numero %2 == 0){
-        
-         printf("O numero %d eh par", numero)
-    }
-    
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &numero);
 
+  if (numero % 2 == 0) {
+    printf("O numero eh par\n");
+  } else {
+    printf("O numero eh impar\n");
+  }
+  numeroCopia = numero;
+  while (numeroCopia > 0) {
+    somadigitos += numeroCopia % 10;
+    numeroCopia /= 10;
+  }
 
+  printf("A soma dos digitos do numero %d eh %d.\n", numero, somadigitos);
 
-
+  return 0;
 }
