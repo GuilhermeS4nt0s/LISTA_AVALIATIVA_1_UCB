f3bdc1e29d64a00a08a99ac256d17c583482669a
Guilherme Santos <113079822+GuilhermeS4nt0s@users.noreply.github.com>
2023-09-28 23:31:49 -0300

    5 finalizado

--- 5ºEXERCICIO.C
+++ 5ºEXERCICIO.C
@@ -2,28 +2,30 @@
 
 int main() {
     int valueone, valuetwo;
-    //printf("digite o primeiro numero\n");
-    scanf("%d %d", &valueone, &valuetwo);
-    if( valueone == valuetwo){
-        prinf("O numero %d e %d sao iguais\n", a,b);
-    }
-    if (valueone > valuetwo) {
-         printf("%d eh maior que %d\n",valueone, valuetwo);
-        if (valueone % valuetwo == 0) {
-            printf("%d eh multiplo de %d\n",valueone, valuetwo);
-        } else {
-            printf("%d nao eh multiplo de %d\n",valueone, valuetwo);
-        }
-    } else if (valueone < valuetwo){
-         printf("%d eh maior que %d\n",valuetwo, valueone);
-        if (valuetwo % valueone == 0) {
-            printf("%d eh multiplo de %d\n",valuetwo, valueone);
-        } else {
-            printf("%d nao eh multiplo de %d\n",valuetwo, valueone);
-        }
-       
-    }else{
+
+    scanf("%d", &valueone);
+    scanf("%d", &valuetwo);
+
+    if (valueone == valuetwo) {
         printf("Os valores lidos sao iguais\n");
+    } else if (valueone > valuetwo) {
+        printf("%d eh maior que %d\n", valueone, valuetwo);
+        if (valueone % valuetwo == 0) {
+            printf("%d eh multiplo de %d\n", valuetwo, valueone);
+        } else {
+            printf("%d nao eh multiplo de %d\n", valuetwo, valueone);
+        }
+    } else if (valuetwo > valueone) {
+        printf("%d eh maior que %d\n", valuetwo, valueone);
+        if (valuetwo % valueone == 0) {
+            printf("%d eh multiplo de %d\n", valueone, valuetwo);
+        } else {
+            if ((valueone == 1 && valuetwo == 0) || (valueone == 0 && valuetwo == 1)) {
+                printf("%d nao eh multiplo de %d\n", valuetwo, valueone);
+            } else {
+                printf("%d nao eh multiplo de %d\n", valueone, valuetwo);
+            }
+        }
     }
     return 0;
 }
